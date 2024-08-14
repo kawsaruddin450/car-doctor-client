48abaef2156d9596fec23c7253f1fbf96154cb0d
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-14 15:17:34 +0600

    delete a checkout functionality

--- src/pages/Bookings/BookingRow.jsx
+++ src/pages/Bookings/BookingRow.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
 
-const BookingRow = ({ booking }) => {
+
+const BookingRow = ({ booking, handleDelete }) => {
     const { _id, name, email, img, title, price } = booking;
+
+    
     return (
         < tr >
             <th>
-                <label>
-                    <input type="checkbox" className="checkbox" />
-                </label>
+                <button onClick={() => handleDelete(_id)} className="btn btn-sm btn-circle btn-neutral">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
             </th>
             <td>
                 <div className="flex items-center gap-3">
--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
 import BookingRow from "./BookingRow";
+import Swal from "sweetalert2";
 
 const Bookings = () => {
     const { user } = useContext(AuthContext);
@@ -10,7 +11,38 @@ const Bookings = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => setBookings(data))
-    }, [])
+    }, []);
+
+    const handleDelete = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/checkouts/${id}`, {
+                    method: "DELETE",
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            const remaining = bookings.filter(booking => booking._id !== id);
+                            setBookings(remaining);
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your booking has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
     return (
         <div className="lg:container mx-auto my-32">
             <h2 className="text-center text-2xl font-bold mb-12">Your bookings: {bookings.length}</h2>
@@ -19,11 +51,7 @@ const Bookings = () => {
                     {/* head */}
                     <thead>
                         <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
+                            <th></th>
                             <th>Service</th>
                             <th>Client</th>
                             <th>Price</th>
@@ -35,6 +63,7 @@ const Bookings = () => {
                             bookings.map(booking => <BookingRow
                             key={booking._id}
                             booking={booking}
+                            handleDelete={handleDelete}
                             ></BookingRow>)
                         }
                     </tbody>
