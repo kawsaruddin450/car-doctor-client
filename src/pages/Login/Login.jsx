408d512dba0b4e0903d04134b1dee036e4e9a34a
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-15 12:52:01 +0600

    redirect from login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import login from '../../assets/images/login/login.svg';
 import { useContext } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
 
 const Login = () => {
     const {signInUser} = useContext(AuthContext);
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
+    const navigate = useNavigate();
 
     const handleLogin = event => {
         event.preventDefault();
@@ -16,6 +19,7 @@ const Login = () => {
         .then(result => {
             const user = result.user;
             console.log(user);
+            navigate(from, {replace: true});
         })
         .catch(error => {
             console.log(error.code);
